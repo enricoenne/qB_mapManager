6b276db88723d7b675e3d9993b7b9402459c93f8
enricoenne <enrico.negri97@gmail.com>
2024-08-07 19:23:23 +0200

    now can convert from the old map format and print maps as .txt in the new format

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import math
 
 def read_map(path):
+    file = open(path, 'r')
+    lines = file.readlines()
+
     return None
-def write_map(path):
+
+def text_to_map(text):
+    lines = text.splitlines()
+
+    n = int(math.sqrt(len(lines)))
+    print(n)
+
+    m = np.zeros((n, n, n))
+
+    for x in range(n):
+        for y in range(n):
+            for z in range(n):
+                if lines[n * x + y][z] == 'w':
+                    m[x, y, z] = 1
+                elif lines[n * x + y][z] == 'o':
+                    m[x, y, z] = 2
+    return m
+
+def write_map(m, path, id):
+    n = m.shape[0]
+    file_name = path + str(n) + "-" + str(id) + ".txt"
+
+    sol = is_solvable(m)
+
+    m = m.astype(int)
+
+    with open(file_name, "w") as file:
+        for slice_2d in m:
+            for row in slice_2d:
+                row_string = ''.join(map(str, row))
+                file.write(row_string + '\n')
+
     return None
 
 def create_map(n, p):
@@ -76,37 +111,58 @@ def is_solvable(m):
                 tree.append((new_pos, depth+1))
     return float("NaN")
 
-size = 5
-N = 10000           # how many maps to generate for each p
-res = 100           # how many p interval to test
-results = np.zeros((res, N))
+def multiple_map_analysis(s = 5, N = 10000, res = 100):
+    size = 5
+    # how many maps to generate for each p
+    # how many p interval to test
+    results = np.zeros((res, N))
 
-for k in range(res):
-    p = k/res
-    for i in range(N):
-        m = create_map(size, p)
-        results[k, i] = is_solvable(m)
-    print(p)
+    for k in range(res):
+        p = k / res
+        for i in range(N):
+            m = create_map(size, p)
+            results[k, i] = is_solvable(m)
+        print(p)
 
-solvable = np.zeros(res)
-solvable_mean = np.zeros(res)
-solvable_median = np.zeros(res)
+    solvable = np.zeros(res)
+    solvable_mean = np.zeros(res)
+    solvable_median = np.zeros(res)
 
-for k in range(res):
-    current_res = results[k,]
-    current_res_solvable = np.invert(np.isnan(current_res))
-    solvable[k] = (current_res[current_res_solvable]).sum()
-    solvable_mean[k] = np.mean(current_res[current_res_solvable])
-    solvable_median[k] = np.median(current_res[current_res_solvable])
+    for k in range(res):
+        current_res = results[k,]
+        current_res_solvable = np.invert(np.isnan(current_res))
+        solvable[k] = (current_res[current_res_solvable]).sum()
+        solvable_mean[k] = np.mean(current_res[current_res_solvable])
+        solvable_median[k] = np.median(current_res[current_res_solvable])
 
+    solvable = solvable / N
 
-solvable = solvable/N
+    plt.plot(solvable_mean, '#00dd00', solvable_median, '#007700', solvable, '#dd0000')
+    plt.show()
 
+    np.savetxt('data.csv', results, delimiter=',')
 
+'''m = create_map(4, 0.25)
+print(is_solvable(m))
 
-plt.plot(solvable_mean, '#00dd00', solvable_median, '#007700', solvable, '#dd0000')
-plt.show()
+write_map(m, "maps/", 1)'''
 
+m = text_to_map(""" w  
+w   
+    
+ww o
+  w 
+www 
+ w  
+ w w
+  w 
+ www
+  ww
+ ww 
+ www
+   w
+w w 
+w  w
+""")
 
-
-np.savetxt('data.csv', results, delimiter=',')
+write_map(m, 'maps/', 'old-4')
